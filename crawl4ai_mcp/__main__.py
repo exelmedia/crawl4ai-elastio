c9535fadf83998478561e4fc388af285f1de64e7
exelmedia <116724277+exelmedia@users.noreply.github.com>
2025-11-21 14:20:05 +0100

    Add files via upload

---
+++ crawl4ai_mcp/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Entry point for running crawl4ai_mcp as a module with python -m crawl4ai_mcp.server
+"""
+
+from crawl4ai_mcp.server import main
+
+if __name__ == "__main__":
+    main()
